f2fdb5a397f4ba299079de716f529fd4e5b14848
CHO YUN SIK <postop09@naver.com>
2022-03-04 14:56:12 +0900

    :lipstick: 리스트 컨포넌트 수정 기능 추가

--- components/List.js
+++ components/List.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 
 // React.memo 는 컴포넌트 랜더링이 될 필요 없는 부분에 적용시켜 준다.
 const List = React.memo(({id, title, completed, todoData, setTodoData, handleClick, provided, snapshot}) => {
-  console.log('List rendered');
   const handleCompleteChange = (id) => {
     let newTodoData = todoData.map((data) => {
       if(data.id === id) {
@@ -13,8 +12,13 @@ const List = React.memo(({id, title, completed, todoData, setTodoData, handleCli
     setTodoData(newTodoData);
   };
   // 목록 내용 수정
+  const [edit, setEdit] = useState(false);
+  const [editText, setEditText] = useState(title);
   const editList = () => {
-    console.log(title);
+    setEdit(!edit);
+  }
+  const handleEditText = (e) => {
+    setEditText(e.target.value);
   }
   
   // key값은 고유한 값을 사용(id값) : key 값을 통해, 바뀐 부분을 인식할 수 있다.
@@ -30,7 +34,7 @@ const List = React.memo(({id, title, completed, todoData, setTodoData, handleCli
           className='mr-2 hover:cursor-pointer'>
         </input>
         {''}
-        <span className={completed ? 'line-through' : undefined}>{title}</span>
+        <p className={completed ? 'line-through' : undefined}>{edit === false ? editText : <input type='text' onChange={handleEditText}/>}</p>
       </div>
       <div className='flex items-center'>
         <button
@@ -38,7 +42,7 @@ const List = React.memo(({id, title, completed, todoData, setTodoData, handleCli
           onClick={() => editList()}
           className='border-2 border-blue-400 rounded text-sm mr-4 p-1 text-blue-400 hover:bg-blue-400 hover:text-white'
         >
-        수정
+        {!edit ? '수정' : '적용'}
         </button>
         <button
           type='button'
