106f88daf5cb1bc6b5ab4a7f9814db5a81c595de
CHO YUN SIK <postop09@naver.com>
2022-03-12 16:07:49 +0900

    Row 컴포넌트 슬라이드 기능 작성 & MovieModal 컴포넌트 작성: 모달 출력/닫기

--- netflix/components/Row.js
+++ netflix/components/Row.js
@@ -1,10 +1,12 @@
+import React, { useEffect, useState } from 'react';
 import axios from '../api/axios';
-import React, { useEffect, useState } from 'react'
-import '../components/Row.css'
-
+import MovieModal from '../components/movieModal/MovieModal';
+import '../components/Row.css';
 
 export default function Row({title, id, url, largeRow}) {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalMovie, setModalMovie] = useState({});
   const fetchMovieData = async () => {
     const req = await axios.get(url);
     // console.log(req);
@@ -15,27 +17,35 @@ export default function Row({title, id, url, largeRow}) {
     fetchMovieData();
   }, []);
 
+  const modalMovieOpen = (movie) => {
+    setModalOpen(true);
+    setModalMovie(movie);
+    console.log(modalMovie);
+  }
+
   return (
     <section className='sec_row'>
       <h2 className='txt_title'>{title}</h2>
       <div className='wrap_slide'>
-        <button type='button' className='btn_left'>
+        <button type='button' onClick={() => {document.getElementById(id).scrollLeft -= window.innerWidth - 80}} className='btn_left'>
           <img src={require('../img/icon_left_arrow.png')} alt='' className='img_arrow img_left' />
         </button>
-        <div className={`wrap_contents ${largeRow && 'wrap_poster'}`}>
+        <div id={id} className={`wrap_contents ${largeRow && 'wrap_poster'}`}>
           {movies.map((movie) => (
             <img
               key={movie.id}
               src={`https://image.tmdb.org/t/p/original/${largeRow ? movie.poster_path : movie.backdrop_path}`}
               alt={movie.title}
+              onClick={() => modalMovieOpen(movie)}
               className={`img_backDrop ${largeRow && 'img_poster'}`}
             />
           ))}
         </div>
-        <button type='button' className='btn_right'>
+        <button type='button' onClick={() => {document.getElementById(id).scrollLeft += window.innerWidth - 80}} className='btn_right'>
           <img src={require('../img/icon_right_arrow.png')} alt='' className='img_arrow img_right' />
         </button>
       </div>
+      {modalOpen && (<MovieModal {...modalMovie} setModalOpen={setModalOpen} />)}
     </section>
   )
 }
---
+++ netflix/components/movieModal/MovieModal.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function MovieModal({id, backdrop_path, first_air_date, name, title, vote_average, overview, setModalOpen}) {
+  
+  return (
+    <Modal key={id}>
+      <ModalContainer>
+        <h3 className='txt_hide'>{name || title} 자세히 보기 모달창</h3>
+        <ModalImgMovie src={`https://image.tmdb.org/t/p/original/${backdrop_path}`} alt='' />
+        <ModalWrapTxt>
+          <ModalTxtDate>출시일: {first_air_date || '정보 없음'}</ModalTxtDate>
+          <ModalTxtTitle>{name || title}</ModalTxtTitle>
+          <ModalTxtVote>평점: {vote_average}</ModalTxtVote>
+          {overview || '설명 정보 없음'}
+        </ModalWrapTxt>
+        <ModalListBtn>
+          <li>
+            <ModalBtn type='button'>PLAY</ModalBtn>
+          </li>
+          <ModalItemListShow>
+            <ModalBtn type='button'>회차 목록 보기</ModalBtn>
+          </ModalItemListShow>
+          <li>
+            <ModalBtn type='button' onClick={() => setModalOpen(false)}>닫기</ModalBtn>
+          </li>
+        </ModalListBtn>
+      </ModalContainer>
+    </Modal>
+  )
+}
+
+const Modal = styled.article`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 1000;
+`
+const ModalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 5px;
+  width: 70vw;
+  min-height: 75vh;
+  background-color: #111;
+`
+const ModalImgMovie = styled.img`
+  width: 100%;
+  object-fit: cover;
+`
+const ModalWrapTxt = styled.p`
+  padding: 20px;
+  font-size: 18px;
+  color: rgb(200, 200, 200);
+`
+const ModalTxtDate = styled.small`
+  display: block;
+  font-size: 16px;
+`
+const ModalTxtTitle = styled.strong`
+  display: block;
+  padding: 10px 0 20px;
+  font-size: 32px;
+  color: white;
+`
+const ModalTxtVote = styled.small`
+  display: block;
+  margin-bottom: 5px;
+  font-size: 18px;
+`
+const ModalListBtn = styled.ul`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  margin-top: auto;
+  padding: 20px;
+`
+const ModalItemListShow = styled.li`
+  margin-left: 20px;
+  margin-right: auto;
+`
+const ModalBtn = styled.button`
+  border: none;
+  border-radius: 3px;
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: rgba(110, 110, 110, 0.8);
+  transition: all .25s;
+  cursor: pointer;
+  &:hover {
+    background-color: rgb(50, 50, 50);
+    color: white;
+  }
+`
